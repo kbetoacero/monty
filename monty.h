7d85c091cc8d2f962cb72f5a8f14fb5960c8b022
kbetoacero <carlos.acero@holbertonschool.com>
2021-05-14 04:44:25 +0000

    monty.h

--- monty.h
+++ monty.h
@@ -1,26 +1,30 @@
-#ifndef MONTY_H
-#define MONTY_H
+#ifndef _MONTY_H_
+#define _MONTY_H_
 
-/*LIBRARIES*/
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-/*GLOBAL VARIABLES*/
-extern int Num_entered;
+/*#include "structs.h""*/
 
-/*STRUCTURES*/
+#define DELIMIT "\n\t\r "
 
 /**
-* struct stack_s - doubly linked list representation of a stack (or queue)
-* @n: integer
-* @prev: points to the previous element of the stack (or queue)
-* @next: points to the next element of the stack (or queue)
-*
-* Description: doubly linked list node structure
-* for stack, queues, LIFO, FIFO Holberton project
-*/
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+
+
 typedef struct stack_s
 {
 	int n;
@@ -29,30 +33,33 @@ typedef struct stack_s
 } stack_t;
 
 /**
-* struct instruction_s - opcode and its function
-* @opcode: the opcode
-* @f: function to handle the opcode
-*
-* Description: opcode and its function
-* for stack, queues, LIFO, FIFO Holberton project
-*/
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
 typedef struct instruction_s
 {
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-/*PROTOTYPE FUNCTIONS*/
+/*extern int ext_n;*/
 
-stack_t *push(stack_t **head, unsigned int line_number);
-stack_t *pall(stack_t **head, unsigned int line_number);
-stack_t *pint(stack_t **head, unsigned int line_number);
-stack_t *pop(stack_t **head, unsigned int line_number);
-stack_t *add(stack_t **stack, unsigned int line_number);
-void open_file(char *file);
-stack_t *(*op_get(char *buff, stack_t **head, unsigned int
-	line_num))(stack_t **head, unsigned int
-	line_number);
-void free_stack(stack_t **stack);
+void push(stack_t **stack, unsigned int line_number, char *n);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
 
-#endif /* MONTY_H */
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
+void pstr(stack_t **stack, unsigned int line_number);
+
+void free_all(stack_t *stack, char *line, FILE *fp);
+
+int struc_opcode(char *opcode, stack_t **stack, unsigned int line_number);
+
+#endif /*_MONTY_H_ */
